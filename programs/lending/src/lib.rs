9f5ed3fd250e4e9b6a43bad397f57e80d29f521b
Anantdadhich <anantdadhich66@gmail.com>
2025-06-11 23:01:53 +0530

    added frontend templeate

--- programs/lending/src/lib.rs
+++ programs/lending/src/lib.rs
@@ -1,16 +1,12 @@
 use anchor_lang::prelude::*;
-
+mod state; 
 declare_id!("BrrQ3W1GLKS7RrL7wpP9UX39en6zkXkvZKEz8ouS8L2w");
 
 #[program]
 pub mod lending {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
+      
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+
