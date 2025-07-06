c7d9da41bcddb3b92b7ca1f920a1426e5a2740af
Anantdadhich <anantdadhich66@gmail.com>
2025-07-06 23:48:24 +0530

    added test files

---
+++ tests/bankrun.spec.ts
@@ -0,0 +1,17 @@
+import { BankrunProvider } from "anchor-bankrun";
+import  {describe ,it} from "node:test"
+
+
+
+//@ts-ignore
+import IDL from "../target/idl/lending.json";
+
+import { Lending } from "../target/types/lending";
+
+
+describe("Lending smart contract tests", ()=>{
+
+     it("initalise the test", async ()=>{
+         
+     })
+})
